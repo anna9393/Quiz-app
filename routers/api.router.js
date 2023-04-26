3841ba59bdfa9c3519355d6503934dbfa2a2f90e
Anna Kašíková <106098004+anna9393@users.noreply.github.com>
2023-04-26 08:49:50 +0200

    Update api.router.js
    create endpoints for Quiz-App

--- routers/api.router.js
+++ routers/api.router.js
@@ -3,5 +3,8 @@ const apiController = require('../controllers/api.controller');
 const apiRouter = express.Router();
 
 apiRouter.get('/game', apiController.game);
+apiRouter.get('/questions', apiController.listQuestions);
+apiRouter.post('/questions', apiController.postQuestion);
+apiRouter.delete('/questions/:id', apiController.deleteQuestion);
 
 module.exports = apiRouter;
